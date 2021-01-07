f791d88f714c06cadbee0c5dc5200c0632e36af4
Matiss Rosset <matiss.rosset7@gmail.com>
2021-01-07 14:09:47 +0100

    Exo 1 à 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,32 @@ import java.util.Scanner;
 
 public class Main {
 
-    /**
-     *
-     * @param args
-     */
+    // Déclaration et initialisation des constantes
+    static final short MAX_PTS_VIE = 100;
+    static final short PTS_BOUCLIER = 25;
+    static final short MAX_ATTAQUE_ENNEMI = 5;
+    static final short MAX_VIE_ENNEMI = 30;
+    static final short MAX_ATTAQUE_JOUEUR = 5;
+    static final short REGENERATION_BOUCLIER_PAR_TOUR = 10;
+
+    //Variables
+    static String nomPersonnage;
+    static short ptsDeVie;
+    static short ptsBouclier;
+    static short nbEnnemisTues = 0;
+    static boolean bouclierActif = true;
+
+    public static void main(String[] args)
+    {
+        initPersonnage();
+    }
+    static void initPersonnage(){
+        System.out.println("Saisir le nom de votre personnage");
+        Scanner scanner = new Scanner(System.in);
+        nomPersonnage = scanner.nextLine();
+        ptsDeVie = MAX_PTS_VIE;
+        ptsBouclier = PTS_BOUCLIER;
+        System.out.println("OK" + Util.color(nomPersonnage, Color.GREEN) + " ! C'est parti")
+    }
 }
+
