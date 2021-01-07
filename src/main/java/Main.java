85a9f3a6641d3aa93dda8ec8b919d3e90ebfe6f8
Matiss Rosset <matiss.rosset7@gmail.com>
2021-01-07 16:53:22 +0100

    Exo 4 à 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-
+    // exercice 1
     // Déclaration et initialisation des constantes
     static final short MAX_PTS_VIE = 100;
     static final short PTS_BOUCLIER = 25;
@@ -10,6 +10,7 @@ public class Main {
     static final short MAX_ATTAQUE_JOUEUR = 5;
     static final short REGENERATION_BOUCLIER_PAR_TOUR = 10;
 
+    // exercice 2
     //Variables
     static String nomPersonnage;
     static short ptsDeVie;
@@ -17,17 +18,53 @@ public class Main {
     static short nbEnnemisTues = 0;
     static boolean bouclierActif = true;
 
-    public static void main(String[] args)
-    {
+    // exercice 3
+    public static void main(String[] args) {
         initPersonnage();
+        short nbPtsEnnemi = attaqueJoueur((short)10);
+        System.out.println("L'ennemi a " + nbPtsEnnemi + " points de vie restant");
     }
-    static void initPersonnage(){
+
+    static void initPersonnage() {
         System.out.println("Saisir le nom de votre personnage");
         Scanner scanner = new Scanner(System.in);
         nomPersonnage = scanner.nextLine();
         ptsDeVie = MAX_PTS_VIE;
         ptsBouclier = PTS_BOUCLIER;
-        System.out.println("OK" + Util.color(nomPersonnage, Color.GREEN) + " ! C'est parti")
+        System.out.println("OK " + Util.color(nomPersonnage, Color.GREEN) + " ! C'est parti !");
+        scanner.close();
+    }
+
+    // exercice 4
+    static boolean hasard(double pourcentageDeChance) {
+        return pourcentageDeChance < Math.random();
+    }
+
+    // exercice 5
+    static short nombreAuHasard(short max) {
+        return (short) Math.round(Math.random() * max);
+    }
+
+    static short attaqueJoueur(short ptsDeVieEnnemi) {
+        short dommages = nombreAuHasard(MAX_ATTAQUE_JOUEUR);
+        System.out.println(Util.color(nomPersonnage, Color.GREEN)
+                + " attaque l'" + Util.color("ennemi", Color.YELLOW) + " ! ");
+        System.out.println("il lui fait perdre " + Util.color(dommages, Color.PURPLE)
+                + " points de dommages");
+        ptsDeVieEnnemi -= dommages;
+        return ptsDeVieEnnemi;
+    }
+    static void afficherPersonnage(){
+        System.out.print(Util.color(nomPersonnage, Color.GREEN) +
+                " (" + Util.color(ptsDeVie, Color.RED));
+        if(bouclierActif){
+            System.out.print(" " + Util.color(ptsBouclier, Color.BLUE));
+        }
+        System.out.print(")");
     }
 }
+
+
+
+
 
